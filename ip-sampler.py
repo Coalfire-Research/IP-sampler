b89fa89601990a2a8ac3fde3cb7eb3c1e83ce0dd
Dan McInerney <danhmcinerney@gmail.com>
2015-03-26 14:46:28 -0600

    fixed multiprocessing

--- ip-sampler.py
+++ ip-sampler.py
@@ -18,6 +18,7 @@ def parse_args():
     parser.add_argument("-p", "--percent", default=5, type=float, help="Enter the percent of IPs per netblock to sample")
     parser.add_argument("-w", "--workers", default=10, type=int, help="Enter the number of paralell workers")
     parser.add_argument("--portscan", action="store_true", help="Instead of doing an ICMP ping sweep, check for at least one of the top 5 ports to respond with SYN/ACK")
+    parser.add_argument("--arpscan", action="store_true", help="Instead of doing an ICMP ping sweep, perform an ARP ping sweep")
     return parser.parse_args()
 
 def main(args):
@@ -27,8 +28,25 @@ def main(args):
     procs = []
     lock = Lock()
     q = Queue()
-    pingtype = args.portscan
     percent = args.percent / float(100)
+    if args.portscan:
+        pingtype = 'portscan'
+    elif args.arpscan:
+        pingtype = 'arpscan'
+    else:
+        pingtype = None
+
+    # Catch CTRL-C
+    def signal_handler(signal, frame):
+        # Stop further processes from spawning
+        q.put('STOP')
+        print '[-] Killing process'
+        for p in procs:
+            p.terminate()
+        if os.path.isfile('ghostdriver.log'):
+            os.remove('ghostdriver.log')
+        q.close()
+    signal.signal(signal.SIGINT, signal_handler)
 
     if not args.hostlist:
         sys.exit('[-] Enter a hostlist file using the -l flag')
@@ -39,15 +57,17 @@ def main(args):
     for l in hosts_list:
         l = l.strip()
         q.put(l)
-    q.put(None)
+    #q.put(None)
 
     # Can't use Pool because pool makes process daemonic which can't produce children
     # NmapProcess is a child of the worker process
     procs = []
     for w in xrange(args.workers):
         p = Process(target=worker, args=(q, lock, percent, pingtype))
+        #p.daemon = True
         p.start()
         procs.append(p)
+        q.put('STOP')
 
     for p in procs:
         p.join()
@@ -62,15 +82,15 @@ def worker(q, lock, percent, pingtype):
     Create Nmap processes to ping sweep each subnet then add a percentage
     of the hosts that are up to the master sample list
     '''
-    while True:
-        netblock = q.get()
-        if netblock == None:
-            return
+    for netblock in iter(q.get, 'STOP'):
 
-        if pingtype:
+        if pingtype == 'portscan':
             nmap_args = '--top-ports 5 --max-rtt-timeout 150ms --max-retries 3'
+        elif pingtype == 'arpscan':
+            nmap_args = '-T4 -sn --max-rtt-timeout 150ms --max-retries 3'
         else:
             nmap_args = '-T4 -PE -sn --max-rtt-timeout 150ms --max-retries 3'
+
         print '[*] nmap {0} {1}'.format(nmap_args, netblock)
         nmap_proc = NmapProcess(targets=netblock, options=nmap_args)
         rc = nmap_proc.run()
@@ -88,7 +108,7 @@ def worker(q, lock, percent, pingtype):
                 hostname = None
                 if len(host.hostnames) != 0:
                     hostname = host.hostnames[0]
-                if pingtype:
+                if pingtype == 'portscan':
                     for s in host.services:
                         if re.search('open|filtered', s.state):
                             subnet_hosts_up.append(ip)
@@ -104,9 +124,10 @@ def worker(q, lock, percent, pingtype):
             s = random.choice(subnet_hosts_up)
             sample.append(s)
 
-        print '[+] Hosts up in subnet {0}:'.format(netblock)
-        for ip in sample:
-            print '     ', ip
+        if len(sample) > 0:
+            print '[+] Random {0}% of live hosts in subnet {1}:'.format(percent*100, netblock)
+            for ip in sample:
+                print '     ', ip
 
         if len(sample) > 0:
             with lock:
